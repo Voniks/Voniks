08401284c52ef62ca11cdcbf934e603d26abbeaf
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-05-30 16:41:25 -0700

    (feat) build basic structure of Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div class="flex-container">
+    <div className="flex-container nav">
+
+      <div className="flex-container nav-left">
+        <div className="nav-element">Logo</div>
+      </div>
+
+      <div className="flex-container nav-right">
+        <div className="nav-element">About</div>
+        <div className="nav-element">Locations</div>
+        <div className="nav-element">Products</div>
+        <div className="nav-element">Catering</div>
+      </div>
 
     </div>
   );
