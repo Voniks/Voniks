68ba79948b7e362e637d3c629db8abb6027e35b2
Jason An <an.jason.1994@gmail.com>
2017-05-30 23:22:08 -0700

    bug/rebase issues

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class Landing extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <header className="landing-header">
         <div className="container">
           <div className="landing">
@@ -16,15 +15,6 @@ class Landing extends Component {
               <li><a href="#catering" className="btn btn-primary" role="button">Catering</a></li>
             </ul>
           </div>
-=======
-      <header>
-        <div className="container landing-header">
-          <ul>
-            <li><a href="#locations" className="btn btn-primary" role="button">Find a Location</a></li>
-            <li><a href="#order" className="btn btn-primary" role="button">Order Now</a></li>
-            <li><a href="#catering" className="btn btn-primary" role="button">Catering Option</a></li>
-          </ul>
->>>>>>> feat/splash button list
         </div>
       </header>
     )
