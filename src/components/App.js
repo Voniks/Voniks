9eec8ca08d200d6b0f6385d42c45a020ea201dd3
Jason An <an.jason.1994@gmail.com>
2017-05-31 14:52:04 -0700

    feat/initialize application sections

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,20 @@ import "../styles/Flex.css";
 class App extends Component {
   render() {
     return (
-      <div className="App-header">
-        <Navbar />
-        <Landing />
+      <div>
+        <div className="App-header">
+          <Navbar />
+          <Landing />
+        </div>
+        <section id="locations">
+          <h3>Locations</h3>
+        </section>
+        <section id="products">
+          <h3>Products</h3>
+        </section>
+        <section id="catering">
+          <h3>Catering</h3>
+        </section>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,8 +10,8 @@ class Landing extends Component {
             <h3>[Slogan]</h3>
             <hr className="landing-divider" />
             <ul className="list-inline landing-buttons">
-              <li><a href="#locations" className="btn btn-primary" role="button">Find a Location</a></li>
-              <li><a href="#order" className="btn btn-primary" role="button">Order Now</a></li>
+              <li><a href="#locations" className="btn btn-primary" role="button">Locations</a></li>
+              <li><a href="#products" className="btn btn-primary" role="button">Products</a></li>
               <li><a href="#catering" className="btn btn-primary" role="button">Catering</a></li>
             </ul>
           </div>
