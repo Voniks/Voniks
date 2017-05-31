0b7ed162d3dd75dad3dcd53574cd64bb77f1ba6f
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-05-31 15:32:29 -0700

    init Products

---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from './Carousel';
+import '../styles/Products.css';
+
+const Products = () => {
+  return (
+    <div id="products" className="container-fluid">
+      <Carousel />
+    </div>
+  );
+};
+
+export default Products;
