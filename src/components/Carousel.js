ac7440d8108712faf82ce1a718fc6346e154eea8
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-05-31 15:49:50 -0700

    add carousel of products

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Slider from 'react-slick';
-import '../styles/Carousel.css';
+import React from "react";
+import Slider from "react-slick";
+import "../styles/Carousel.css";
 
 const Carousel = () => {
   return (
     <div id="carousel">
-      <Slider draggable={true} slidesToShow={4} adaptiveHeight={true}>
-        <img src="https://lh3.googleusercontent.com/nYhPnY2I-e9rpqnid9u9aAODz4C04OycEGxqHG5vxFnA35OGmLMrrUmhM9eaHKJ7liB-=w300" />
+      <Slider
+        infinite={false}
+        draggable={true}
+        slidesToShow={4}
+        adaptiveHeight={true}
+      >
+        <img src="http://www.prevention.com/sites/prevention.com/files/images/news/featured_images/citrus-juice-TS-158268808-628.jpg" />
+        <img src="http://www.tasteforlife.com/sites/default/files/styles/desktop/public/field/image/detoxjuices.png?itok=oe2Ke9Pg0" />
+        <img src="http://www.mediterraneandietforall.com/wp-content/uploads/2014/01/mediterranean-diet-juices.jpg" />
+        <img src="https://www.wagamama.us/-/media/WagamamaUSMainsite/hero-pod-images/juices.jpg" />
       </Slider>
     </div>
   );
