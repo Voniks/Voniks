0d5e7455957057fab5e2ced731ea78874b67b7d7
Jason An <an.jason.1994@gmail.com>
2017-06-01 16:31:08 -0700

    feat/initialize contact.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Catering from "./Catering";
+import Contact from "./Contact";
 import Navbar from "./Navbar";
 import Landing from "./Landing";
 import Products from "./Products";
@@ -24,6 +25,10 @@ class App extends Component {
         <section id="catering">
           <h3>Catering</h3>
           <Catering />
+        </section>
+        <section id="contact">
+          <h3>Contact Us</h3>
+          <Contact />
         </section>
       </div>
     );
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h3>hi</h3>
+      </div>
+    );
+  }
+}
+
+export default Contact;
