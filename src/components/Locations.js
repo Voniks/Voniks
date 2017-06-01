d77288f5fa8107db43bc1411fea5025e327767f0
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-06-01 16:14:12 -0700

    init locations

---
+++ src/components/Locations.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Locations.css";
+
+const Locations = () => {
+  return (
+    <div className="fluid-container">
+
+    </div>
+  );
+};
+
+export default Locations;
