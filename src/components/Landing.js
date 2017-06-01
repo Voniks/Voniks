d231ee43efd9f7cc7c62bd81eb19ef3770f1b345
Jason An <an.jason.1994@gmail.com>
2017-05-31 17:53:19 -0700

    feat/landing resize

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,9 +10,9 @@ class Landing extends Component {
             <h3>[Slogan]</h3>
             <hr className="landing-divider" />
             <ul className="list-inline landing-buttons">
-              <li><a href="#locations" className="btn btn-primary" role="button">Locations</a></li>
-              <li><a href="#products" className="btn btn-primary" role="button">Products</a></li>
-              <li><a href="#catering" className="btn btn-primary" role="button">Catering</a></li>
+              <li><a href="#locations" className="btn btn-primary btn-lg" role="button">Locations</a></li>
+              <li><a href="#products" className="btn btn-primary btn-lg" role="button">Products</a></li>
+              <li><a href="#catering" className="btn btn-primary btn-lg" role="button">Catering</a></li>
             </ul>
           </div>
         </div>
