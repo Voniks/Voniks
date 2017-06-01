cdd35412b83d58a01347c5d894ae3a712b8107c7
Jason An <an.jason.1994@gmail.com>
2017-05-31 17:54:43 -0700

    refactor/ removed div

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,16 +5,14 @@ class Landing extends Component {
     return (
       <header className="landing-header">
         <div className="container">
-          <div className="landing">
-            <h1>Welcome to Voniks!</h1>
-            <h3>[Slogan]</h3>
-            <hr className="landing-divider" />
-            <ul className="list-inline landing-buttons">
-              <li><a href="#locations" className="btn btn-primary btn-lg" role="button">Locations</a></li>
-              <li><a href="#products" className="btn btn-primary btn-lg" role="button">Products</a></li>
-              <li><a href="#catering" className="btn btn-primary btn-lg" role="button">Catering</a></li>
-            </ul>
-          </div>
+          <h1>Welcome to Voniks!</h1>
+          <h3>[Slogan]</h3>
+          <hr className="landing-divider" />
+          <ul className="list-inline landing-buttons">
+            <li><a href="#locations" className="btn btn-primary btn-lg" role="button">Locations</a></li>
+            <li><a href="#products" className="btn btn-primary btn-lg" role="button">Products</a></li>
+            <li><a href="#catering" className="btn btn-primary btn-lg" role="button">Catering</a></li>
+          </ul>
         </div>
       </header>
     )
