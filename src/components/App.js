c847d06989657b62a346b7ba0bf90074936520bb
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-06-01 16:13:56 -0700

    add locations to App

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Contact from "./Contact";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Landing from "./Landing";
+import Locations from "./Locations";
 import Products from "./Products";
 import "../styles/App.css";
 import "../styles/Flex.css";
@@ -18,6 +19,7 @@ class App extends Component {
         </div>
         <section id="locations">
           <h3>Locations</h3>
+          <Locations />
         </section>
         <section id="products">
           <h3>Products</h3>
