3559c41ec0ab01c2838c953dee336b5581a184fd
Jason An <an.jason.1994@gmail.com>
2017-06-01 21:14:18 -0700

    feat/add description for catering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Logo from "../images/voniks-logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-inverse navbar-static-top">
+    <nav className="navbar navbar-inverse navbar-fixed-top">
       <div className="container-fluid">
         <div id="hamburger-menu" className="navbar-header pull-right">
           <button
