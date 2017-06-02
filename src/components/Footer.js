489be75b1d12ca051b8ab3eb1313d708ad02263b
Jason An <an.jason.1994@gmail.com>
2017-06-01 19:29:07 -0700

    feat/operating hours

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,25 @@ class Footer extends Component {
         <footer>
           <div className="container">
             <div className="row">
-              <div className="col-md-4">
+              <div className="col-md-3">
                 <span className="copyright">© 2014 by Voniks Juice LLC</span>
               </div>
-              <div className="col-md-4">
+              <div className="col-md-3">
                 <ul className="list-inline social-buttons">
                   <li><a href="https://www.facebook.com/Voniksfoods/"><i className="fa fa-facebook-square"></i></a></li>
                   <li><a href="https://www.instagram.com/voniksfoods/"><i className="fa fa-instagram"></i></a></li>
                   <li><a href="https://twitter.com/VoniksJuice"><i className="fa fa-twitter"></i></a></li>
                 </ul>
               </div>
-              <div className="col-md-4">
-                <a href="tel:1-301-963-8218"><i className="glyphicon glyphicon-earphone"></i><p>1-301-963-8218</p></a>
+              <div className="col-md-3">
+                <a href="tel:1-301-963-8218"><i className="glyphicon glyphicon-earphone"></i><p>Tel:1-301-963-8218</p></a>
+                <p>Fax: 1-610-887-7127</p>
+                <a href="MAILTO:guestservice@voniksjuice.com"><p>Email: guestservice@voniksjuice.com</p></a>
+              </div>
+              <div className="col-md-3">
+                <p><strong>Operating Hours:</strong></p>
+                <p>Mon-Fri: 10am-9pm</p>
+                <p>Sat-Sun: 9am-9pm</p>
               </div>
             </div>
           </div>
