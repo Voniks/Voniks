3a0f7043c6410e94d9107de383b5db3067e8475c
Jason An <an.jason.1994@gmail.com>
2017-06-01 17:55:40 -0700

    refactor/ footer spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,27 @@ import "./../styles/Footer.css";
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <span className="copyright">© 2014 by Voniks Juice LLC</span>
-            </div>
-            <div className="col-md-4">
-              <ul className="list-inline social-buttons">
-                <li><a href="https://www.facebook.com/Voniksfoods/"><i className="fa fa-facebook-square"></i></a></li>
-                <li><a href="https://www.instagram.com/voniksfoods/"><i className="fa fa-instagram"></i></a></li>
-                <li><a href="https://twitter.com/VoniksJuice"><i className="fa fa-twitter"></i></a></li>
-              </ul>
-            </div>
-            <div className="col-md-4">
-              <a href="tel:1-301-963-8218"><i className="glyphicon glyphicon-earphone"></i><p>1-301-963-8218</p></a>
+      <div className="footer">
+        <footer>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <span className="copyright">© 2014 by Voniks Juice LLC</span>
+              </div>
+              <div className="col-md-4">
+                <ul className="list-inline social-buttons">
+                  <li><a href="https://www.facebook.com/Voniksfoods/"><i className="fa fa-facebook-square"></i></a></li>
+                  <li><a href="https://www.instagram.com/voniksfoods/"><i className="fa fa-instagram"></i></a></li>
+                  <li><a href="https://twitter.com/VoniksJuice"><i className="fa fa-twitter"></i></a></li>
+                </ul>
+              </div>
+              <div className="col-md-4">
+                <a href="tel:1-301-963-8218"><i className="glyphicon glyphicon-earphone"></i><p>1-301-963-8218</p></a>
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </div>
     );
   }
 }
