083fbb4a5b058a5658036f7cb8c38e684e535bcd
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-06-01 17:27:27 -0700

    add media screens for Carousel images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,10 +11,18 @@ const Carousel = () => {
         slidesToShow={4}
         adaptiveHeight={true}
       >
-        <img src="http://www.prevention.com/sites/prevention.com/files/images/news/featured_images/citrus-juice-TS-158268808-628.jpg" />
-        <img src="http://www.tasteforlife.com/sites/default/files/styles/desktop/public/field/image/detoxjuices.png?itok=oe2Ke9Pg0" />
-        <img src="http://www.mediterraneandietforall.com/wp-content/uploads/2014/01/mediterranean-diet-juices.jpg" />
-        <img src="https://www.wagamama.us/-/media/WagamamaUSMainsite/hero-pod-images/juices.jpg" />
+        <div className="carousel-img-container">
+          <img src="http://www.prevention.com/sites/prevention.com/files/images/news/featured_images/citrus-juice-TS-158268808-628.jpg" />
+        </div>
+        <div className="carousel-img-container">
+          <img src="http://www.tasteforlife.com/sites/default/files/styles/desktop/public/field/image/detoxjuices.png?itok=oe2Ke9Pg0" />
+        </div>
+        <div className="carousel-img-container">
+          <img src="http://www.mediterraneandietforall.com/wp-content/uploads/2014/01/mediterranean-diet-juices.jpg" />
+        </div>
+        <div className="carousel-img-container">
+          <img src="https://www.wagamama.us/-/media/WagamamaUSMainsite/hero-pod-images/juices.jpg" />
+        </div>
       </Slider>
     </div>
   );
