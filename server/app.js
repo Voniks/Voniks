e6c70103aa0d7bde8c3c2edd7d87526664987dce
Jason An <an.jason.1994@gmail.com>
2017-06-01 21:43:20 -0700

    refactor/not json formatted

--- server/app.js
+++ server/app.js
@@ -4,9 +4,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.post('/catering', (req, res) => {
+  console.log(req.body);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
