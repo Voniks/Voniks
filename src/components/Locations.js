79bdbc55c75980ea7528e74dc487d733c49dcbf6
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-06-02 19:41:22 -0700

    add current store locations

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -4,18 +4,14 @@ import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 const GoogleMapComp = withGoogleMap(props => (
   <GoogleMap
     ref={props.onMapLoad}
-    defaultZoom={5}
-    defaultCenter={{ lat: 38.889931, lng: -77.009003 }}
-    onClick={props.onMapClick}
+    defaultZoom={10}
+    defaultCenter={{ lat: 39, lng: -77.1 }}
     defaultOptions={{
-      scrollwheel: false,
+      scrollwheel: false
     }}
   >
     {props.markers.map((marker, index) => (
-      <Marker
-        {...marker}
-        onRightClick={() => props.onMarkerRightClick(index)}
-      />
+      <Marker {...marker} onClick={marker => props.onMarkerClick(marker)} />
     ))}
   </GoogleMap>
 ));
@@ -27,18 +23,24 @@ class Locations extends Component {
       markers: [
         {
           position: {
-            lat: 25.0112183,
-            lng: 121.52067570000001
+            lat: 38.9097,
+            lng: -77.0433
           },
-          key: `Taiwan`,
+          key: "Dupont Circle, Washington",
+          defaultAnimation: 2
+        },
+        {
+          position: {
+            lat: 39.1159,
+            lng: -77.1976
+          },
+          key: "Washingtonian Center in Gaithersburg, Maryland",
           defaultAnimation: 2
         }
       ]
     };
-
+    this.onMarkerClick = this.onMarkerClick.bind(this);
     this.handleMapLoad = this.handleMapLoad.bind(this);
-    this.handleMapClick = this.handleMapClick.bind(this);
-    this.handleMarkerRightClick = this.handleMarkerRightClick.bind(this);
   }
 
   handleMapLoad(map) {
@@ -48,54 +50,18 @@ class Locations extends Component {
     }
   }
 
-  /*
-   * This is called when you click on the map.
-   * Go and try click now.
-   */
-  handleMapClick(event) {
-    const nextMarkers = [
-      ...this.state.markers,
-      {
-        position: event.latLng,
-        defaultAnimation: 2,
-        key: Date.now() // Add a key property for: http://fb.me/react-warning-keys
-      }
-    ];
-    this.setState({
-      markers: nextMarkers
-    });
-
-    if (nextMarkers.length === 3) {
-      this.props.toast(
-        `Right click on the marker to remove it`,
-        `Also check the code!`
-      );
-    }
-  }
-
-  handleMarkerRightClick(targetMarker) {
-    /*
-     * All you modify is data, and the view is driven by data.
-     * This is so called data-driven-development. (And yes, it's now in
-     * web front end and even with google maps API.)
-     */
-    const nextMarkers = this.state.markers.filter(
-      marker => marker !== targetMarker
-    );
-    this.setState({
-      markers: nextMarkers
-    });
+  onMarkerClick(marker) {
+    this._mapComponent.panTo(marker.latLng);
   }
 
   render() {
     return (
       <GoogleMapComp
-        containerElement={<div style={{ height: "100vh" }} />}
-        mapElement={<div style={{ height: "100vh" }} />}
+        containerElement={<div style={{ height: "80vh" }} />}
+        mapElement={<div style={{ height: "80vh" }} />}
         onMapLoad={this.handleMapLoad}
-        onMapClick={this.handleMapClick}
         markers={this.state.markers}
-        onMarkerRightClick={this.handleMarkerRightClick}
+        onMarkerClick={this.onMarkerClick}
       />
     );
   }
